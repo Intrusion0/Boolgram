d7a0dee764581dcaeaca1119a9491f10860c451a
Andreea1696 <andreeapetrescu16@gmail.com>
2022-03-29 17:46:10 +0200

    Import Story component into Stories

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -1,16 +1,39 @@
 <template>
     <section id="container-stories">
-        SONO STORIES
+        <!-- SONO STORIES -->
+        <Story 
+        v-for="story, i in stories"
+        :key="i"
+        :details="story"/>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
+import Story from './Story.vue';
+
 export default {
   name: 'Stories',
+  components: {
+    Story,
+  },
 
   data() {
     return {
+      stories: [],
+    }
+  },
 
+  created() {
+    this.getStories();
+  },
+
+  methods: {
+    getStories() {
+      axios.get('https://flynn.boolean.careers/exercises/api/boolgram/profiles')
+          // .then((r) => console.log(r = r.data))
+          .then((r) => this.stories = r.data)
+          .catch((e) => console.error(e));
     }
   }
 }
@@ -21,9 +44,14 @@ export default {
 // Container stories
 #container-stories {
     width: 100%;
+    border: 2px solid #efefef;
+    padding: 20px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
     // debug
-    height: 100px;
-    background-color: aquamarine;
+    // height: 100px;
+    // background-color: aquamarine;
 }
 
 </style>
