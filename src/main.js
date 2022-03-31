2941957122194a409fdc75b9e187c0fcadf0d40f
Mario <lombardomario01@gmail.com>
2022-03-31 19:07:06 +0200

    Add Vue-js-modal Package

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import moment from 'moment'
+import VModal from 'vue-js-modal'
 
 require('moment/locale/it');
 
 moment.locale('it');
 
 Vue.config.productionTip = false
+
+Vue.use(VModal);
 
 new Vue({
   render: h => h(App),
